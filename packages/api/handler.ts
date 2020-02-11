fed0f836c34d3af06fd3226cc262269ff813f350
Jonatan <jgranqvist@gmail.com>
2020-02-11 07:33:24 +0100

    Lol!

--- packages/api/handler.ts
+++ packages/api/handler.ts
@@ -1,10 +1,24 @@
-export async function hello(event, context, callback) {
+export async function hello (event, context, callback) {
   await new Promise((resolve, reject) => setTimeout(resolve, 500))
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event
+    })
+  }
+
+  callback(null, response)
+}
+
+export async function lol (event, context, callback) {
+  await new Promise((resolve, reject) => setTimeout(resolve, 500))
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Lool!',
       input: event
     })
   }
